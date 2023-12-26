ff0489d1106dd158b6d5e8cc6e76cb75baf726d4
Sivakumar2146 <113508223+Sivakumar2146@users.noreply.github.com>
2023-12-26 15:22:46 +0530

    Create Reverse String.java

---
+++ Reverse String.java
@@ -0,0 +1,45 @@
+package Java_IV_Q;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// 1) using + (string concatenation ) operator
+		String str="SIVA";
+		String rev="";
+		
+	/*	int len=str.length();  //4
+           
+		for (int i=len-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+			
+		}
+	
+	System.out.println("Revered string is here:"  +   rev);
+	
+	*/
+	
+	//2 using character  array
+		
+		
+		char a[]=str.toCharArray();
+		int len=a.length;
+		for (int i=len-1;i>=0;i--)
+		{
+			rev=rev+a[i];
+		}
+		System.out.println("Revered string is here:"  +   rev);	
+		
+	
+   	StringBuffer sb=new StringBuffer(str);
+    System.out.println(	sb.reverse());
+		
+		
+	}
+	
+	
+
+}
